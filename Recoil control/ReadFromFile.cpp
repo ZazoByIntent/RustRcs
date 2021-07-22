3f0f7b74794ff6b056853da1e4c1028a82ea5deb
Aljaž Hafner <zazo2001@gmail.com>
2021-07-22 17:55:03 +0200

    Updated location

--- Recoil control/ReadFromFile.cpp
+++ Recoil control/ReadFromFile.cpp
@@ -3,7 +3,7 @@
 
 
 std::vector<float> getSettings() {
-	std::ifstream file(R"(C:\Users\hafneral\Desktop\rcs.txt)");
+	std::ifstream file(R"(C:\Users\zazo2\Desktop\rcs.txt)");
 	std::vector<float> set;
 	float sens, fov, mult, rndm, timer, aftimer;
 	file >> sens; set.push_back(sens);
@@ -16,14 +16,14 @@ std::vector<float> getSettings() {
 
 
 float getRndm() {
-	std::ifstream file(R"(C:\Users\hafneral\Desktop\rcs.txt)");
+	std::ifstream file(R"(C:\Users\zazo2\Desktop\rcs.txt)");
 	float rndm;
 	file >> rndm; file >> rndm; file >> rndm; file >> rndm;
 	return rndm;
 }
 
 float getTimer() {
-	std::ifstream file(R"(C:\Users\hafneral\Desktop\rcs.txt)");
+	std::ifstream file(R"(C:\Users\zazo2\Desktop\rcs.txt)");
 	float timer;
 	file >> timer; file >> timer; file >> timer; file >> timer; file >> timer;
 	return timer;
